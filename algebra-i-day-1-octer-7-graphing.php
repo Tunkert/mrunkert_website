225a2c6c477fe1adeed2a7f4a368791749b18f5d
Tunkert <tunkert@gmail.com>
2023-03-19 15:23:35 -0600

    finished alg I day 1

--- algebra-i-day-1-octer-7-graphing.php
+++ algebra-i-day-1-octer-7-graphing.php
@@ -9,10 +9,21 @@ $description = "Learn how to graph on the coordinate plane (xy-plane)!";
 
 <main class="mt-5">
 	<article>
+		<div id="top"></div>
 		<h1 class="text-center">Graphing on the Coordinate Plane</h1>
 		<div class="container">
 			<div class="row">
 				<div class="col-12">
+					<section id="table-of-contents">
+						<h2>Table of Contents</h2>
+						<ul>
+							<li><a href="#example-1">Example 1</a></li>
+							<li><a href="#example-2">Example 2</a></li>
+							<li><a href="#example-3">Example 3</a></li>
+							<li><a href="#example-4">Example 4</a></li>
+							<li><a href="#conclusion">Conclusion</a></li>
+						</ul>
+					</section>
 					<p>Being able to graph points on a coordinate plane is a necesary skill for high school math and beyond. In this lesson you'll learn exactly how to do that ... LIKE A BOSS. I can't help myself ...</p>				
 					<p>Let's look at some examples.</p>
 					<section id="example-1">
@@ -36,7 +47,39 @@ $description = "Learn how to graph on the coordinate plane (xy-plane)!";
 						<p>This will bring you to the point (-2, 0).</p>
 						<p>You'll now need to go up 3 units. This will bring you to the point (-2, 3). Plot the point.</p>
 						<p>Here is an image of the point plotted below:</p>
+						<figure>
+							<img src="images/coordinate-plane/pointneg23.png" alt="The point (-2, 3) on the coordinate plane.">
+							<figcaption>Photo courtesy of <a href="https://www.desmos.com/calculator" target="_blank">Desmos Graphing</a></figcaption>
+						</figure>
 					</section>
+					<section id="example-3">
+						<h2>Example 3 - Graphing on a Coordinate Plane</h2>
+						<p>Graph the point (-4, -4) on the coordinate plane.</p>
+						<h3>Strategy and Solution</h3>
+						<p>We first have to go 4 units left of the origin to get to the point (-4, 0).</p>
+						<p>We then go down 4 units to get to (-4, -4).</p>
+						<p>Plot the point there.</p>
+						<figure>
+							<img src="images/coordinate-plane/pointneg4neg4.png" alt="The point (-4, -4) on the coordinate plane.">
+							<figcaption>Photo courtesy of <a href="https://www.desmos.com/calculator" target="_blank">Desmos Graphing</a></figcaption>
+						</figure>
+					</section>
+					<section id="example-4">
+						<h2>Example 4 - Graphing on the Coordinate Plane</h2>
+						<p>Graph the point (5, -1) on the coordinate plane.</p>
+						<h3>Strategy and Solution</h3>
+						<p>Since the x-value is now positive, we need to go five units to the right from the origin.</p>
+						<p>We then go down 1 unit to the point (5, -1) and plot the point.</p>
+						<figure>
+							<img src="images/coordinate-plane/point5neg1.png" alt="The point (5, -1) on the coordinate plane.">
+							<figcaption>Photo courtesy of <a href="https://www.desmos.com/calculator" target="_blank">Desmos Graphing</a></figcaption>
+						</figure>
+					</section>
+					<section id="conclusion">
+						<h2>Conclusion</h2>
+						<p>Graphing points on the coordinate plane is easy if you remember that you need to move left or right first, depending on the x-value of the point. After that, you need to move up or down depending on the y-value. Thanks for reading.</p>
+					</section>
+					<p><a href="#top">Go to top.</a></p>
 				</div>
 			</div>
 		</div>
---
+++ algebra-ii-day-1-octer-7-direct-variation.php
@@ -0,0 +1,30 @@
+<?php
+
+$title = "Direct Variation";
+$description = "Learn about direct variation in this article.";
+
+?>
+
+<?php include "includes/header.php"; ?>
+
+<main>
+	<article>
+		<div class="container">
+			<h1 class="text-center">Direct Variation</h1>		
+			<p>A direct variation is a proportional relationship in which two values have a direct relationship with one another. In other words, if one value increases the other value increases proportionally. An example of direct variation could be hours worked and paycheck amount for <i>hourly employees</i>. Another example might be the speed of a plane and how far you fly.</p>
+			<p>To see direct variation in a mathematical setting let's look at some examples:</p>
+			<section id="example-1">
+				<h2>Example 1</h2>
+				<p>Which of the following is an example of direct variation:</p>
+				<ul>
+					<li>ab = 5</li>
+					<li>a = 5b</li>
+					<li>a = <sup>5</sup>/<sub>b</sub></li>
+					<li>ab = <sup>1</sup>/<sub>3</sub></li>
+				</ul>
+			</section>
+		</div>
+	</article>
+</main>
+
+<?php include "includes/footer.php"; ?>
