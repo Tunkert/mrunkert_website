e069d421d2ae1173b239e922636084b22cd4a3d2
tunkert <tunkert@gmail.com>
2023-03-20 19:57:57 -0600

    Algebra I day 2 lesson

---
+++ algebra-i-quadrants.php
@@ -0,0 +1,72 @@
+<?php
+        
+$title = "Algebra I Quadrants | MrUnkert.com";
+$description = "In this post you'll learn all you need to know about the Quadrants of the coordinate plane.";
+?>
+
+<?php include "includes/header.php"; ?>
+
+<main>
+    <article>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>The Quadrants of the Coordinate Plane</h1>
+                    <p>There are four quadrants in the coordinate plane starting with the upper right hand corner of the graph. This is known as Quadrant I. We then cycle through the remaining three quadrants in a counter-clockwise position, LIKE A BOSS.</p>
+                    <p>Quadrant II is the the upper left corner of the graph. The x-values are negative here and the y-values are positive still.</p>
+                    <p>Cycling counter-clockwise to Quadrant III both the x and y-values are negative and cycling into the 4th Quadrant, which is the lower left hand corner yields positive x-values but negative y-values.</p>
+                    <p>In the picture below you have points on each of the four quadrants - like a boss.</p>
+                    <figure>
+                        <img src="images/coordinate-plane/pointsInQuadrants.png" alt="points plotted in each of the four quadrants on the coordinate plane.">
+                        <figcaption>Photo courtesy of <a href="https://www.desmos.com/calculator" target="_blank">Desmos Graphing</a></figcaption>
+                    </figure>
+                    <p>Let's practice plotting points and placing them on the coordinate plane.</p>
+                    <section id="example-1">
+                        <h2>Example 1</h2>
+                        <p>Plot the point (3, 4) and label the Quadrant it is in.</p>
+                        <h3>Strategy and Solution</h3>
+                        <p>We'll first start out by plotting the point. To do that we'll need to go three right and then four up. (Always do x first and then y). Since both x and y are positive and we're in the upper right hand corner of the graph we're in Quadrant I.</p>
+                        <figure>
+                            <img src="images/coordinate-plane/point34QuadrantOne.png" alt="The point (3, 4) in Quadrant One">
+                            <figcaption>Photo courtesy of <a href="https://www.desmos.com/calculator/" target="_blank">Desmos Graphing</a></figcaption>
+                        </figure>
+                    </section>
+                    <section id="example-2">
+                        <h2>Example 2</h2>
+                        <p>Plot the point (-1, 2) and label the Quadrant it is in.</p>
+                        <h3>Strategy and Solution</h3>
+                        <p>We'll first move 1 to the left, since x is negative, then 2 up, since y is positive. We'll plot the point here. Since the x-value is negative and the y-value is positive we know that we're in Quadrant II:</p>
+                        <figure>
+                            <img src="images/coordinate-plane/pointNeg12QuadrantTwo.png" alt="The point (-1, 2) in Quadrant II">
+                            <figcaption>Photo courtesy of <a href="https://www.desmos.com/calculator" target="_blank">Desmos Graphing.</a></figcaption>
+                        </figure>
+                    </section>
+                    <section id="example-3">
+                        <h2>Example 3</h2>
+                        <p>Graph (-2, -4) and label the Quadrant it's in.</p>
+                        <h3>Strategy and Solution</h3>
+                        <p>We'll first move left 2 units and then go down 2 units. Both the x and y-values are negative so we know that we're in Quadrant III. </p>
+                        <figure>
+                            <img src="images/coordinate-plane/pointNeg24Quadrant3.png" alt="The point (-2, -4) in Quadrant III">
+                            <figcaption>Photo courtesy of <a href="https://www.desmos.com/calculator/" target="_blank">Desmos Graphing</a></figcaption>
+                        </figure>
+                    </section>
+                    <section id="example-4">
+                        <h2>Example 4</h2>
+                        <p>Plot the point (3, -4) on the coordinate plane and identify the Quadrant that (3, -4) is in.</p>
+                        <figure>
+                            <img src="images/coordinate-plane/point3Neg4Quadrant4.png" alt="The point (3, -4) in Quadrant IV.">
+                            <figcaption>Photo courtesy of <a href="https://www.desmos.com/calculator/" target="_blank">Desmos Graphing</a></figcaption>
+                        </figure>
+                    </section>
+                    <section id="conclusion">
+                        <h2>Conclusion</h2>
+                        <p>Now you know how to plot points and the four Quadrants of the coordinate plane ... hopefully. I could have done this conclusion better ... LIKE A BOSS.</p>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </article>
+</main>
+
+<?php include "includes/footer.php"; ?>
