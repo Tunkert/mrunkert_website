7d65a9360b6b1cf1eb06f9d08a3286abe5af38bb
tunkert <tunkert@gmail.com>
2023-03-20 20:27:00 -0600

    Algebra II day 2 lesson

---
+++ algebra-ii-inverse-variation.php
@@ -0,0 +1,116 @@
+<?php
+
+$title = "Algebra II Inverse Variation | MrUnkert.com";
+$description = "In this post you'll learn about inverse variation LIKE A BOSS.";
+
+?>
+
+<?php include "includes/header.php"; ?>
+
+<main>
+    <article>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Inverse Variation</h1>
+                    <p>Inverse variation is a proportional relationship where one value increases as the other value decreases. A generic equation that represents inverse variation as y = <sup>k</sup>/<sub>x</sub>. </p>
+                    <p>Let's look at some examples:</p>
+                    <section id="example-1">
+                        <h2>Example 1</h2>
+                        <p>Which of the following represents inverse variation?</p>
+                        <ul>
+                            <li class="lower-alpha">a = 5b</li>
+                            <li class="lower-alpha"><sup>a</sup>/<sub>b</sub> = 5</li>
+                            <li class="lower-alpha"><sup>b</sup>/<sub>a</sub> = <sup>1</sup>/<sub>5</sub></li>
+                            <li class="lower-alpha">a = <sup>5</sup>/<sub>b</sub></li>
+                        </ul>
+                        <h3>Solution</h3>
+                        <p>In this case d is the correct solution as when b increases a will decrease.</p>
+                        <p>A table will help us visualize this:</p>
+                        <table border="1" cellpadding="10" cellspacing="0">
+                            <tr>
+                                <th>a</th>
+                                <th>b</th>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td>1</td>
+                            </tr>
+                            <tr>
+                                <td>2.5</td>
+                                <td>2</td>
+                            </tr>
+                            <tr>
+                                <td>1.67</td>
+                                <td>3</td>
+                            </tr>
+                        </table>
+                        <p>We can see from the table that as a halves (2.5), then is cut into thirds (1.67), b is doubled then tripled. Hence the inverse variation. LIKE A BOSS.</p>
+                    </section>
+                    <section id="example-2">
+                        <h2>Example 2</h2>
+                        <p>Which of the following represents inverse variation?</p>
+                        <p>Note: k equals 3.</p>
+                        <ul>
+                            <li class="lower-alpha">yx = k</li>
+                            <li class="lower-alpha">y = kx</li>
+                            <li class="lower-alpha"><sup>y</sup>/<sub>x</sub> = k</li>
+                            <li class="lower-alpha">y = k + x</li>
+                        </ul>
+                        <h3>Strategy and Solution</h3>
+                        <p>In this case yx = k is a perfect example of direct variation. If we divide both sides by x, we get y = <sup>k</sup>/<sub>x</sub>, the generic version of direct variation I mentioned above.</p>
+                        <p>In class, we'll make a table with the value of k also.</p>
+                    </section>
+                    <section id="example-3">
+                        <h2>Example 3</h2>
+                        <p>Which of the following represents inverse variation?</p>
+                        <ul>
+                            <li class="lower-alpha">x - y = 2</li>
+                            <li class="lower-alpha">x + y = 2</li>
+                            <li class="lower-alpha">xy = 2</li>
+                            <li class="lower-alpha">x = 2 - y</li>
+                        </ul>
+                        <h3>Solution</h3>
+                        <p>xy = 2 is our example of inverse variation here. Let's make a table to demonstrate.</p>
+                        <table border="1" cellpadding="10" cellspacing="0">
+                            <tr>
+                                <th>x</th>
+                                <th>y</th>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>2</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>1</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>0.5</td>
+                            </tr>
+                        </table>
+                        <p>We can see here that as x doubles, y halves. As x is quadrupled, y is a fourth of it's original value.</p>
+                    </section>
+                    <section id="example-4">
+                        <h2>Example 4</h2>
+                        <p>Which of the following represents inverse variation?</p>
+                        <ul>
+                            <li class="lower-alpha">p = 5r</li>
+                            <li class="lower-alpha"><sup>p</sup>/<sub>r</sub> = 5</li>
+                            <li class="lower-alpha">pr = 5</li>
+                            <li class="lower-alpha">5 = 7</li>
+                        </ul>
+                        <p>In this case we can throw out answer d, that's just ridiculous. a is an example of direct variation, so it can't be inverse variation as is example b. Only c is an example of inverse variation. We'll make a table of this one in class ... LIKE A BOSS.</p>
+                    </section>
+                    <section id="conclusion">
+                        <h2>Conclusion</h2>
+                        <p>Remember that direct variation is a proportional relationship where as one value increases the other value increases proportionally. Inverse variation is where as one value increases the other value decreases proportionally. When in doubt make a table to figure which is which.</p>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </article>
+</main>
+
+<?php include "includes/footer.php; ?>
